e24e47a11007a4d4a96ae7aec9e0346c9d21daaa
Joseph <86561660+JosephCss@users.noreply.github.com>
2026-06-02 22:14:50 -0500

    login
    Added AuthApp class for user authentication and screen management.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
-
+from login.login_screen import LoginScreen
 from admin.admin_screen import AdminScreen
+from login.register_screen import RegisterScreen
 from admin.product_manager import ProductScreen
+from login.profile_screen import ProfileScreen
 from admin.orders_screen import OrdersScreen
 
-
+class AuthApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Light'
+        self.theme_cls.primary_palette = 'Pink'
+        
+        # Crear ScreenManager
+        sm = ScreenManager()
+        
+        # Agregar pantallas
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(RegisterScreen(name='register'))
+        sm.add_widget(ProfileScreen(name='profile'))
+        
+        return sm
+        
 class MainApp(App):
 
     def build(self):
@@ -28,3 +45,4 @@ class MainApp(App):
 
 
 MainApp().run()
+AuthApp().run()
